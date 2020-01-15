940a7e931626a8f3c84972067a7dbf1c1b916796
villatrue <scarter0313@gmail.com>
2020-01-15 17:48:39 -0500

    added material ui

---
+++ src/containers/HomeContainer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Header from '../components/Header'
+
+export default function HomeContainer() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container >
+        <Header></Header>
+        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
+        
+      </Container>
+    </React.Fragment>
+  );
+}
